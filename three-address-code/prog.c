55dcde681de76f84b460cffcfd2d6e7ad16b4a8d
sameeriron42 <sameermark42@gmail.com>
2023-05-11 11:36:00 +0530

    output in the format of backend compiler to parse

--- three-address-code/prog.c
+++ three-address-code/prog.c
@@ -24,7 +24,7 @@ int main(){
     strcpy(temp,"t");
 
     if(strcmp(s[3].data,"+")==0 || !strcmp(s[3].data,"-"))
-        printf("%s=%s%s%s\n",s[current_expr_no].temp,s[2].data,s[3].data,s[4].data);
+        printf("%s = %s %s %s\n",s[current_expr_no].temp,s[2].data,s[3].data,s[4].data);
     
     current_expr_no++;
     for(i=4;i<max_expr_no-2;i+=2)
@@ -38,10 +38,10 @@ int main(){
         strcpy(temp,"t");        
 
         if(strcmp(s[i+1].data,"+")==0 || !strcmp(s[i+1].data,"-"))
-            printf("%s=%s%s%s\n",s[current_expr_no].temp,s[current_expr_no-1].temp,s[i+1].data,s[i+2].data);
+            printf("%s = %s %s %s\n",s[current_expr_no].temp,s[current_expr_no-1].temp,s[i+1].data,s[i+2].data);
     }
 
     
-    printf("%s=%s\n",s[0].data,s[current_expr_no-1].temp);
+    printf("%s = %s\n",s[0].data,s[current_expr_no-1].temp);
     return 0;
 }
